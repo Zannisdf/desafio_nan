adff274dbab46746087fde1de729b6b7d197680a
Giorgio Zanni <g.zanni@hotmail.com>
2019-02-13 15:46:12 -0300

    FIX: Person occupation field

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :people
   devise_for :admins
+
+  root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
